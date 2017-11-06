0d8f9daa09fae1e401779a9133e8d6380d00e52c
220-TomiyamaAkifumi <akifumi_tomiyama@tbn.co.jp>
2017-11-06 16:47:03 +0900

    work

--- setup.sh
+++ setup.sh
@@ -5,9 +5,17 @@ cd ${DOT_DIRECTORY}
 for f in .??*
 do
   # ignore file
+  [[ ${f} = ".bash_profile" ]] && continue
   [[ ${f} = ".git" ]] && continue
   [[ ${f} = ".gitignore" ]] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
-echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
-
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)o
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${HOME}/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${HOME}/.bash_profile
+echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ${HOME}/.bash_profile
+echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> ${HOME}/.bash_profile
+echo 'eval "$(rbenv init -)"'  >> ${HOME}/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile
+echo 'alias vim="nvim"' >> ${HOME}/.bash_profile
+exec $SHELL -l
