19fc9d2391b0debaf21207fb0ee7f9cd87967547
tomisuker <tomisuker16@gmail.com>
2017-11-06 21:58:27 +0900

    works w/ deoplete

--- setup.sh
+++ setup.sh
@@ -11,11 +11,3 @@ do
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)o
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${HOME}/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${HOME}/.bash_profile
-echo 'export RBENV_ROOT="$HOME/.rbenv"' >> ${HOME}/.bash_profile
-echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> ${HOME}/.bash_profile
-echo 'eval "$(rbenv init -)"'  >> ${HOME}/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile
-echo 'alias vim="nvim"' >> ${HOME}/.bash_profile
-exec $SHELL -l
