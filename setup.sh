d658e4cc3a7673e9530292339316ad31db64c31c
tomisuker <tomisuker16@gmail.com>
2017-12-23 16:55:56 +0900

    typo

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ cd ${DOT_DIRECTORY}
 for f in .??*
 do
   # ignore file
-  [[ ${f} = ".bash_profile" ]] && continue
-  [[ ${f} = ".git" ]] && continue
-  [[ ${f} = ".gitignore" ]] && continue
+  [ ${f} = ".bash_profile" ] && continue
+  [ ${f} = ".git" ] && continue
+  [ ${f} = ".gitignore" ] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)o
