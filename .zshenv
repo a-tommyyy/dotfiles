659b5e340264854fb987d9d6c93a47648d995765
tomisuker <tomisuker16@gmail.com>
2018-07-31 02:10:19 +0900

    update

--- .zshenv
+++ .zshenv
@@ -1,4 +1,15 @@
-# Example aliases
+#
+# Defines environment variables.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Ensure that a non-login, non-interactive shell has a defined environment.
+if [[ ( "$SHLVL" -eq 1 && ! -o LOGIN ) && -s "${ZDOTDIR:-$HOME}/.zprofile" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprofile"
+fi
+
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 
@@ -20,4 +31,3 @@ fi
 if [ $SHLVL = 1 ]; then
     alias tmux="tmux -2 attach || tmux -2 new-session"
 fi
-
--- link.sh
+++ link.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && \
 ZSH_CUSTOM="$ZSH/custom"
-
-if git --version 2>&1 >/dev/null ; then
-  # spaceship zsh theme
-  mkdir "$ZSH_CUSTOM/themes/spaceship-prompt"
-  git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" && \
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-  # zsh-auto-suggestion
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-
-  # zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-fi
 
 DOT_DIRECTORY="${HOME}/dotfiles"
 cd ${DOT_DIRECTORY}
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 for f in .??*
 do
@@ -39,14 +27,6 @@ do
         ln -sv ${DOT_DIRECTORY}/.config/${f} ${HOME}/.config/${f}
     fi
 done
-
-#if [ "$(uname)" == 'Darwin' ]; then
-#  OS='Mac'
-#elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-#  OS='Linux'
-#else
-#  echo "Platform ($(uname-a)) is not supported."
-#fi
 
 cd ${DOT_DIRECTORY}
 
